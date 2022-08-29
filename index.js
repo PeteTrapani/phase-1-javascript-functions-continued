122cb5fa5a67c89a3f18c861ab1b2c99dde6a41d
Pete Trapani <pete.trapani91@gmail.com>
2022-08-29 12:57:00 -0500

    commit tree 6bed2561a20e394096d8970a975c8871cbdc96d2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(job = `roller-skate`) {
+    return (`This Saturday, I want to ${job}!`);
+}
+saturdayFun();
+saturdayFun(`bath my dog`);
+
+function mondayWork(activity = 'go to the office') {
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork()
+mondayWork(`dog`)
